97316374c6265f934eaa40d16e6bc41fd61c0acf
mhws2 <mhws2@MHWS2>
2020-08-03 20:56:39 +0800

    添加项目文件。

---
+++ 15PB-Snake/15PB-Snake.cpp
@@ -0,0 +1,14 @@
+﻿/*  15PB-Snake.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+    IDE     :   Visual_Studio_2019
+    Time    :   20200803-20:00
+    By      :   Hongwei.Mao
+    Coding  :   UTF-8
+    Ver     :   V1.0
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
