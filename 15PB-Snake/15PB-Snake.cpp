35e2b0412d443c80abe7a2ac15d4ca70d4a22a63
mhws2 <mhws2@MHWS2>
2020-08-05 18:04:20 +0800

    更新

--- 15PB-Snake/15PB-Snake.cpp
+++ 15PB-Snake/15PB-Snake.cpp
@@ -19,19 +19,21 @@
 #define KUp 72
 #define KUp 72
 #define KLeft 75
-#define KRight 77;
+#define KRight 77
 #define KDown 80
-#define SHead 'A'
-#define SBody 'B'
+#define MAPSHead 'A'
+#define MAPSBody 'B'
 using namespace std;
 
 const char apple[][3] = {"※","　" ,"◆","◇","①","②","③" };
 static char map[MAP_H][MAP_W] = { 0 };
 
 typedef struct _SNKBODY {
+    unsigned int i;
     unsigned short x;
     unsigned short y;
     struct _SNKBODY* NEXT;
+    struct _SNKBODY* PREV;
 }SNKBODY;
 
 class CSnake
@@ -39,43 +41,108 @@ class CSnake
 public:
     CSnake();
     ~CSnake();
+    void CRefresh(char cOrientatio = '\0', bool IsPrint = false);
 
 private:
     unsigned short x;
     unsigned short y;
+    unsigned char orientation;
+    unsigned int bodylen;
     SNKBODY* body;
+    SNKBODY* bodyEND;
 };
 
 CSnake::CSnake()
 {
+    cout << "实例化CSnake" << endl;
     this->x = 2;
     this->y = 0;
+    this->bodylen = 2;
+    this->orientation = 'D';
     this->body = new SNKBODY;
-    this->body->NEXT = nullptr;
+    SNKBODY* tmp = new SNKBODY;
+    tmp->NEXT = nullptr;
+    tmp->x = 1;
+    tmp->y = 0;
+    tmp->i = 1;
+    this->body->NEXT = tmp;
+    tmp = new SNKBODY;
+    tmp->NEXT = nullptr;
+    tmp->x = 0;
+    tmp->y = 0;
+    tmp->i = 2;
+    this->body->NEXT->NEXT = tmp;
+    this->CRefresh();
 }
 
 CSnake::~CSnake()
 {
-    delete body;
+    delete this->body;
+}
+
+void CSnake::CRefresh(char cOrientation,bool IsPrint)
+{
+    int x2, y2;                                 //申请移动后的坐标
+    switch (cOrientation)
+    {
+    case 'W':
+        if (this->orientation != 'S')
+            this->orientation = cOrientation;
+        break;
+    }
+    x2 = this->x + 1; y2 = this->y;
+    if (x2 >= MAP_W || y2 >= MAP_H) {           //超出地图边界
+        cout << "撞墙啦！" << endl;
+        return;
+    }
+    switch (this->orientation) {
+    default:break;
+    }
+    SNKBODY* tmp = this->body;                  //初始化链表循环
+    while (tmp->NEXT != nullptr) {              //循环链表，将地图清空
+        tmp = tmp->NEXT;                        //跳过链表头并循环
+        map[tmp->y][tmp->x] = '0';              //将地图数组赋值0
+        if (tmp->i == this->bodylen - 1)
+            this->bodyEND = tmp;                //并且将尾赋值给END
+    }
+    if (map[y2][x2] != '1') {
+        tmp->x = this->x; tmp->y = this->y;     //将蛇尾移动至蛇头
+        tmp->NEXT = this->body->NEXT;           //重定义蛇体
+        this->body->NEXT = tmp;                 //重定义链表
+        this->bodyEND->NEXT = nullptr;          //重定义链表尾
+    }
+    map[this->y][this->x] = MAPSHead;           //画蛇头
+    tmp = this->body; this->bodylen = 0;        //初始化链表
+    while (tmp->NEXT != nullptr) {              //循环链表，将地图赋值
+        tmp = tmp->NEXT;
+        tmp->i = ++this->bodylen;               //重新计算蛇身长
+        map[tmp->y][tmp->x] = MAPSBody;         //将数组赋值
+    }
+    //if (IsPrint) ::MAPprint();                    //传入是否打印参数
 }
 
 
 void MAPprint()
 {
-    cout << apple[0]<<endl;
-    for (unsigned short i = 0, tmp = 0; i < MAP_H; i++)
+    unsigned short i = MAP_W + 2, tmp, j = 0;
+    while (i-->0)
+        cout << apple[0];
+    cout << "\n";
+    for (i=0; i < MAP_H; i++)
     {
         cout << apple[0];
-        for (unsigned short j = 0; j < MAP_W; j++)
+        for (j = 0; j < MAP_W; j++)
         {
             switch (map[i][j]){
-            case SHead: tmp = 2; break;
-            case SBody: tmp = 3; break;
+            case MAPSHead: tmp = 2; break;
+            case MAPSBody: tmp = 3; break;
             default:    tmp = 1; break;}
             cout << apple[tmp];
-        }   cout << apple[0] << endl;
+        }   cout << apple[0] << "\n";
     }
-    cout << apple[0] << endl;
+    for (i = MAP_W + 2; i > 0; --i)
+        cout << apple[0];
+    cout << endl;
 }
 
 int main()
@@ -90,11 +157,10 @@ int main()
         switch (key)
         {
         case KLeft:case 65:case 97:
-            //std::cout << "左\n";
-            map[1][10] = 'B';
-            map[1][9] = 'A';
-            MAPprint();
-            break;
+            MAPprint();break;
+        case KRight:
+            cSnke.CRefresh('D', true); MAPprint(); break;
+        case 224:break;
         default:
             std::cout << "键码\t"<<key << std::endl;
             break;
